4adecd5ecdca241bb3e7c43eeecf548433d57aaa
Apple Cider <quangminh29102007@gmail.com>
2026-04-30 16:35:14 +0700

    Add position and movement methods to Shape class
    Added position attributes and movement methods for the Shape class.

--- Shape.java
+++ Shape.java
@@ -3,9 +3,28 @@ import java.awt.Color;
 public class Shape {
     public int[][] coordinates;
     public Color color;
+    public int x;
+    public int y;
 
     public Shape(int[][] coords, Color col) {
         this.coordinates = coords;
         this.color = col;
+    }
+
+    public void spawn(int startX, int startY) {
+        this.x = startX;
+        this.y = startY;
+    }
+
+    public void moveLeft() {
+        this.x--;
+    }
+
+    public void moveRight() {
+        this.x++;
+    }
+
+    public void moveDown() {
+        this.y++;
     }
 }
